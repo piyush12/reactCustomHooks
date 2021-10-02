86e7bf5368a7d1be1a79ff0efb69c5a1613264e0
piyush12 <pcpiyus480@gmail.com>
2021-10-02 23:46:39 +0530

    useIsMounted Hook

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # reactCustomHooks
-Created with CodeSandbox
+
+# useThrottle
+
+```
+  const [value, setValue] = useState("");
+const [throtlledvalue, throttledEventHandle] = useThrottle(value, 1500);
+
+const handleChange = (event) => {
+  setValue(event.target.value);
+  throttledEventHandle(event.target.value);
+};
+
+useEffect(() => {
+  console.log("throttledValue", throtlledvalue);
+}, [throtlledvalue]);
+
+return (
+  <div className="App">
+    <h1>Hello CodeSandbox</h1>
+    <h2>Start editing to see some magic happen!</h2>
+    <input type="text" value={value} onChange={handleChange} />
+  </div>
+);
+
+```
+
+# useIsMounted
+
+```
+const isMounted = useIsMounted();
+
+  useEffect(() => {
+    if (isMounted) {
+      console.log("Mounted");
+    }
+  }, [isMounted]);
+
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import { useEffect, useState } from "react";
-import useThrottle from "./hooks/useThrottle";
+import useIsMounted from "./hooks/useIsMounted";
 import "./styles.css";
 
 export default function App() {
-  const [value, setValue] = useState("");
-  const [throtlledvalue, throttledEventHandle] = useThrottle(value, 1500);
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-    throttledEventHandle(event.target.value);
-  };
+  const isMounted = useIsMounted();
 
   useEffect(() => {
-    console.log("throttledValue", throtlledvalue);
-  }, [throtlledvalue]);
+    if (isMounted) {
+      console.log("Mounted");
+    }
+  }, [isMounted]);
 
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <input type="text" value={value} onChange={handleChange} />
     </div>
   );
 }
